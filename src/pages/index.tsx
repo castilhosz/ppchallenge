c1418294ee29df8d37f2e7dc53e987570dc6d1ef
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-19 14:47:28 -0300

    fix: next structure page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/pages/Home.module.css'
 
-import Header from "./components/Header";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (

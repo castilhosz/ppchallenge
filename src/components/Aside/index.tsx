ce5c15e16a41d486add41446706a696d7fac4d15
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-21 21:51:39 -0300

    feat: separate aside from homepage

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Aside: React.FC = () => {
+  return (<aside className={styles.container} />);
+}
+
+export default Aside;
